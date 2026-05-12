43daceefa8e45c6964bbb5c163792b2570e50c4b
hyeonseo <hyeonseo@nethru.co.kr>
2026-05-12 21:28:11 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/sellfriend/SellFriendServerApplication.java
@@ -0,0 +1,13 @@
+package com.sellfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellFriendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellFriendServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sellfriend/SellFriendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellFriendServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
