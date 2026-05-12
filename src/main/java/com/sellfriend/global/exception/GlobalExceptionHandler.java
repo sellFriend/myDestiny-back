3e05b1bb39b5da2c0919404cb83ebe504db0acf1
hyeonseo <hyeonseo@nethru.co.kr>
2026-05-12 21:36:11 +0900

    initial commit

---
+++ src/main/java/com/sellfriend/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.sellfriend.global.exception;
+
+import com.sellfriend.global.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidation(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+            .map(error -> error.getField() + ": " + error.getDefaultMessage())
+            .findFirst()
+            .orElse("입력값이 유효하지 않습니다.");
+        return ResponseEntity.badRequest().body(ApiResponse.fail(message));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        return ResponseEntity.internalServerError().body(ApiResponse.fail(e.getMessage()));
+    }
+}
